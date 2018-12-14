3a3593cb80086ff78f9d00f9155999d08d2f8b04
kenreurisondca <kenreurison@dca.ufrn.br>
2018-12-14 15:27:07 -0200

    Teste com o display7Segmentos. Aparece valores de 0 - 9.

--- display7segment.vhd
+++ display7segment.vhd
@@ -10,12 +10,12 @@ architecture display7segment of display7segment is
  
 begin
  
-displays(0) <= B0 OR B2 OR (B1 AND B3) OR (NOT B1 AND NOT B3);
-displays(1) <= (NOT B1) OR (NOT B2 AND NOT B3) OR (B2 AND B3);
-displays(2) <= B1 OR NOT B2 OR B3;
-displays(3) <= (NOT B1 AND NOT B3) OR (B2 AND NOT B3) OR (B1 AND NOT B2 AND B3) OR (NOT B1 AND B2) OR B0;
-displays(4) <= (NOT B1 AND NOT B3) OR (B2 AND NOT B3);
-displays(5) <= B0 OR (NOT B2 AND NOT B3) OR (B1 AND NOT B2) OR (B1 AND NOT B3);
-displays(6) <= B0 OR (B1 AND NOT B2) OR ( NOT B1 AND B2) OR (B2 AND NOT B3);
+displays(0) <= not(B0 OR B2 OR (B1 AND B3) OR (NOT B1 AND NOT B3));
+displays(1) <= not((NOT B1) OR (NOT B2 AND NOT B3) OR (B2 AND B3));
+displays(2) <= not(B1 OR NOT B2 OR B3);
+displays(3) <= not((NOT B1 AND NOT B3) OR (B2 AND NOT B3) OR (B1 AND NOT B2 AND B3) OR (NOT B1 AND B2) OR B0);
+displays(4) <= not((NOT B1 AND NOT B3) OR (B2 AND NOT B3));
+displays(5) <= not(B0 OR (NOT B2 AND NOT B3) OR (B1 AND NOT B2) OR (B1 AND NOT B3));
+displays(6) <= not(B0 OR (B1 AND NOT B2) OR ( NOT B1 AND B2) OR (B2 AND NOT B3));
  
 end display7segment;
